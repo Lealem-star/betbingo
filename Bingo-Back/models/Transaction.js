3b6116bc0b6ff202acc4bbb1576eb9d415395875
Lealem-1 <lealem.meseret@aastustudent.edu.et>
2025-11-03 23:55:58 +0300

    bonus transaction

--- Bingo-Back/models/Transaction.js
+++ Bingo-Back/models/Transaction.js
@@ -8,7 +8,7 @@ const transactionSchema = new mongoose.Schema({
     },
     type: {
         type: String,
-        enum: ['deposit', 'withdrawal', 'game_bet', 'game_win', 'game_completion', 'coin_conversion', 'bonus', 'refund', 'wallet_transfer', 'invite_reward'],
+        enum: ['deposit', 'withdrawal', 'game_bet', 'game_win', 'game_completion', 'coin_conversion', 'bonus', 'refund', 'wallet_transfer', 'invite_reward', 'registration_bonus'],
         required: true
     },
     amount: {
